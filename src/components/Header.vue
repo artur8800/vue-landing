4695c5f9108ce7fc0c3bd8140f6c5186f52c475f
Artur8800 <artursviridonov@gmail.com>
2021-12-20 04:26:23 +0200

    Base project set-up

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="page__header">
+    <div class="container">
+      <div class="header__logo">
+        <img class="header__logo" src="../assets/img/Logo.svg" />
+      </div>
+      <nav class="header__navigation">
+        <ul class="header__navigation__list">
+          <li class="header__navigation__item">
+            <a class="header__navigation__link" href="#">About me</a>
+          </li>
+          <li class="header__navigation__item">
+            <a class="header__navigation__link" href="#">Relationships</a>
+          </li>
+          <li class="header__navigation__item">
+            <a class="header__navigation__link" href="#">Requirements</a>
+          </li>
+          <li class="header__navigation__item">
+            <a class="header__navigation__link" href="#">Users</a>
+          </li>
+          <li class="header__navigation__item">
+            <a class="header__navigation__link" href="#">Sign Up</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default { name: "Header" };
+</script>
